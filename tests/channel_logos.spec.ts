6022307f5369bb7f770f6c7bd5a8d6af5ff46cf2
Katanpe <95223903+Katanpe@users.noreply.github.com>
2023-04-18 14:47:36 +0300

    Add files via upload
    added accessibility testing

--- tests/channel_logos.spec.ts
+++ tests/channel_logos.spec.ts
@@ -1,4 +1,18 @@
 import { test, expect } from '@playwright/test';
+const AxeBuilder = require('@axe-core/playwright').default;
+
+test('should not have any automatically detectable accessibility issues', async ({ page }, testInfo) => {
+    await page.goto('https://areena.yle.fi/tv/opas');
+  
+    const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
+  
+      await testInfo.attach('accessibility-scan-results', {
+        body: JSON.stringify(accessibilityScanResults, null, 2),
+        contentType: 'application/json'
+      });
+  
+    expect(accessibilityScanResults.violations).toEqual([]);
+});
 
 test('has Yle TV1 logo and label', async ({ page }) => {
   await page.goto('https://areena.yle.fi/tv/opas');
